64c80052f1bc150859314990ee030f8f48fd27de
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-06-12 03:49:44 +0200

    Home Button Fix

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -10,12 +10,31 @@ import { TokenService } from 'src/app/modules/auth/token/token.service';
 })
 export class ToolbarComponent implements OnInit{
 
-  constructor(private auth: AuthService){}
+
+  constructor(private auth: AuthService, private router: Router, private token: TokenService){}
+
   public logged: boolean = false;
+  public role: string = "";
+
   ngOnInit(): void {
     this.auth.userState$.subscribe( (result) =>{
       this.logged = result;
     })
   }
+
+  homeRoute() {
+    if(this.logged){
+      this.role = this.token.getUser().role;
+
+      if(this.role === "ROLE_PASSENGER"){
+        this.router.navigate(["/request-ride"]);
+
+      }else if(this.role === "ROLE_DRIVER"){
+        this.router.navigate(["/driver-home"])
+      }
+    }
+    }
+  
+    
 
 }
